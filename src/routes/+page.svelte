00c68cc4af69dddad09ebeca5f2b027342551571
Jens Pots <jens@jenspots.com>
2024-03-18 18:20:44 +0100

    chore: init sass

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style lang="sass">
+    main
+        @apply p-16 space-y-1
+    h1
+        @apply text-2xl font-semibold
+</style>
